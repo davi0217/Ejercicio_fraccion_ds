061489cdb789f8f5528969f189ee82c8f724500e
davi0217 <sanchez.david@bootcamp-upgrade.com>
2025-01-25 12:06:52 +0100

    nueva correccion javadoc
    Corregimos las indicaciones necesarias para elaborar un javadoc

--- src/ejercicio_fraccion/Fraccion.java
+++ src/ejercicio_fraccion/Fraccion.java
@@ -9,7 +9,7 @@ public class Fraccion {
      * Incluye métodos getters y setters y también de devolución de fracción como
      * texto
      * 
-     * davidsanchez@authoros
+     * @author davidsanchez
      * 
      * @version 1.0
      */
@@ -18,25 +18,21 @@ public class Fraccion {
     private int denominador;
 
     /**
-     * Los atributos numerador y denominador son números enteros
+     * La función constructora es una función que admite sobrecarga
+     * En este caso, al no recibir parámetros, devuelve la fracción 0/1
      */
-
     public Fraccion() {
-        /**
-         * La función constructora es una función que admite sobrecarga
-         * En este caso, al no recibir parámetros, devuelve la fracción 0/1
-         */
         this.numerador = 0;
         this.denominador = 1;
     }
 
+    /**
+     * Cuando la función constructora recibe dos parámetros, valida que el
+     * denominador no sea 0
+     * En ese caso, crea una nueva función con el numerador y el denominador
+     * indicados
+     */
     public Fraccion(int num, int den) {
-        /**
-         * Cuando la función constructora recibe dos parámetros, valida que el
-         * denominador no sea 0
-         * En ese caso, crea una nueva función con el numerador y el denominador
-         * indicados
-         */
         if (den != 0) {
             this.numerador = num;
             this.denominador = den;
@@ -45,39 +41,39 @@ public class Fraccion {
         }
     }
 
+    /**
+     * Si el constructor solo recibe el numerador, asigna el valor 1 al denominador
+     */
     public Fraccion(int num) {
-        /**
-         * Si el constructor solo recibe el numerador, asigna el valor 1 al denominador
-         */
         this.numerador = num;
         this.denominador = 1;
     }
 
+    /**
+     * Devuelve el valor del numerador
+     */
     public int getNumerador() {
-        /**
-         * Devuelve el valor del numerador
-         */
         return this.numerador;
     }
 
+    /**
+     * Cambia el valor del numerador
+     */
     public void setNumerador(int num) {
-        /**
-         * Cambia el valor del numerador
-         */
         this.numerador = num;
     }
 
+    /**
+     * Devuelve el valor del denominador
+     */
     public int getDenominador() {
-        /**
-         * Devuelve el valor del denominador
-         */
         return this.denominador;
     }
 
+    /**
+     * Cambia el valor del denominador, validando que no sea 0
+     */
     public void setDenominador(int den) {
-        /**
-         * Cambia el valor del denominador, validando que no sea 0
-         */
         if (den != 0) {
             this.denominador = den;
         } else {
@@ -85,19 +81,19 @@ public class Fraccion {
         }
     }
 
+    /**
+     * Devuelve la fracción com texto
+     */
     public String fraccionTexto() {
-        /**
-         * Devuelve la fracción com texto
-         */
 
         return (Integer.toString(numerador) + "\\" + Integer.toString(denominador));
     }
 
+    /**
+     * Una serie de impresiones para asegurar el correcto funcionamiento de la
+     * función
+     */
     public static void main(String[] args) throws Exception {
-        /**
-         * Una serie de impresiones para asegurar el correcto funcionamiento de la
-         * función
-         */
         Fraccion fraccion = new Fraccion(2, 5);
         System.out.println("El denominador de mi primera fracción es " + fraccion.getDenominador());
         System.out.println("El numerador de mi primera fracción es " + fraccion.getNumerador());
